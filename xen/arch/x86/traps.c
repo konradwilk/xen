7293e0b8e0a4bf0622045f403fb4fe869f62269f
Feng Wu <feng.wu@intel.com>
2014-06-03 11:29:38 +0200

    x86: make set_nmi_callback return the old nmi callback
    This patch makes set_nmi_callback return the old nmi callback, so
    we can set it back later.
    
    Signed-off-by: Feng Wu <feng.wu@intel.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    (cherry picked from commit 238faa9daf9f3dff782128302c5fd923e49f2143)
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- xen/arch/x86/traps.c
+++ xen/arch/x86/traps.c
@@ -3248,9 +3248,13 @@ void do_nmi(struct cpu_user_regs *regs)
     }
 }
 
-void set_nmi_callback(nmi_callback_t callback)
+nmi_callback_t set_nmi_callback(nmi_callback_t callback)
 {
+    nmi_callback_t old_nmi_callback = nmi_callback;
+
     nmi_callback = callback;
+
+    return old_nmi_callback;
 }
 
 void unset_nmi_callback(void)
