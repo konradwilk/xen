e3255759840b92cfd4cdd7f8a5ce83e23801ee57
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2015-09-16 10:37:12 -0400

    x86/xen_hello_world.xsplice: Test payload for patching 'xen_extra_version'.
    This change demonstrates how to generate an xSplice ELF payload.
    
    The idea here is that we want to patch in the hypervisor
    the 'xen_version_extra' function with an function that will
    return 'Hello World'. The 'xl info | grep extraversion'
    will reflect the new value after the patching.
    
    To generate this ELF payload file we need:
     - C code of the new code (xen_hello_world_func.c).
     - C code generating the .xsplice.funcs structure
       (xen_hello_world.c)
     - The address of the old code (xen_extra_version). We
       retrieve it by  using 'nm --defined' on xen-syms.
     - The size of the new and old code for which we use
       nm --defined -S on our code and xen-syms respectively.
    
    There are two C files and one header files generated
    during build. One could make this one C file if the
    size of the newly patched function size was known in
    advance (or an random value was choosen).
    
    There is also a strict order of compiling:
     1) xen_hello_world_func.c
     2) config.h - extract the size of the new function,
        the old function and the old function address.
     3) xen_hello_world.c - which contains the .xsplice.funcs
        structure.
     4) Link the object files in an xen_hello_world.xsplice file.
    
    The use-case is simple:
    
    $xen-xsplice load /usr/lib/debug/xen_hello_world.xsplice
    $xen-xsplice list
     ID                                     | status
    ----------------------------------------+------------
    xen_hello_world                           APPLIED
    $xl info | grep extra
    xen_extra              : Hello World
    $xen-xsplice revert xen_hello_world
    Performing revert: completed
    $xen-xsplice unload xen_hello_world
    Performing unload: completed
    $xl info | grep extra
    xen_extra              : -unstable
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Julien Grall <julien.grall@arm.com> [ARM]
    ---
    Cc: Stefano Stabellini <stefano.stabellini@citrix.com>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Keir Fraser <keir@xen.org>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    
    v2: Do it using hypervisor Makefiles
    v3: Remove the stale linker file.
        Add Copyright and local definition block
        s/name/xen_hello_world_name/
    v6: Remove the 'install', and 'uninstall' destinations.
        Remove xen/config.h from files.
    v7: Made the build target be called 'tests'.
        Changed the .name to have 'xen_extra_version' to be consistent
        with the spec.
        Add Julien's Ack and Andrew's Reviewed-by.
    ---

---
+++ xen/arch/x86/test/xen_hello_world.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
+ *
+ */
+
+#include "config.h"
+#include <xen/types.h>
+#include <xen/xsplice.h>
+
+static char hello_world_patch_this_fnc[] = "xen_extra_version";
+extern const char *xen_hello_world(void);
+
+struct xsplice_patch_func __section(".xsplice.funcs") xsplice_xen_hello_world = {
+    .version = XSPLICE_PAYLOAD_VERSION,
+    .name = hello_world_patch_this_fnc,
+    .new_addr = (unsigned long)(xen_hello_world),
+    .old_addr = OLD_CODE,
+    .new_size = NEW_CODE_SZ,
+    .old_size = OLD_CODE_SZ,
+};
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
