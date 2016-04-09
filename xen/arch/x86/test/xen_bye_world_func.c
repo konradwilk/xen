59110fafcd12c678ecdc739f447ad1ed55330f74
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2016-04-09 13:44:44 -0400

    xsplice: Stacking build-id dependency checking.
    We now expect that the ELF payloads be built with the
    --build-id.
    
    Also the .xsplice.deps section has to have the contents
    of the hypervisor (or a preceding payload) build-id.
    
    We already have the code to verify the Elf_Note build-id
    so export parts of it.
    
    This dependency means the hypervisor MUST be compiled with
    --build-id - so we gate the build of xSplice on the availability
    of said functionality.
    
    This does not impact the ordering of how the payloads can
    be loaded, but it does enforce an STRICT ordering when the
    payloads are applied. Also the REPLACE is special - we need
    to check that its dependency against the hypervisor - not
    the last applied patch.
    
    To make this easier to test we also add an extra test-case
    to be used - which can only be applied on top of the
    xen_hello_world payload.
    
    As in, one can apply xen_hello_world and then xen_bye_world
    on top of that. Not the other way.
    
    We also print the dependency and payloads build_in the keyhandler.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    
    ---
    Cc: Keir Fraser <keir@xen.org>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    
    v3: First time included.
    v4: Andrew fix against the build_id.o mutilations.
        Andrew fix to not include extra symbols in binary.id
    v5: s/ssize_t/unsigned int/
    v6: s/an NT_GNU../a NT_GNU/
       - Squash "xsplice: Print dependency and payloads build_id in the keyhandler"
         in this patch.
       - Add in xen_build_id_check size of section for better checking.
    v7: Added Andrew's reviewed-by.
        Change the .name in test-case to adhere to spec.
        Dropped NT_GNU_BUILD_ID and moved that to earlier patch
        (build_id: Provide ld-embedded build-ids)
        Amended spec and code to only have one of .xsplice.depends and
        .note.gnu.build-id
        Expanded comment about note.o and why we don't use arch/x86/note.o.bin
        Moved xen_build_id_check definition to xsplice.h from version.h
        (and dropping the #include's in version.h)
        Sort header files in tests.
    v8:
     - Change two of the dprinkt from XENLOG_DEBUG to XENLOG_ERR
    ---

---
+++ xen/arch/x86/test/xen_bye_world_func.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
+ *
+ */
+
+#include <xen/types.h>
+#include <asm/nops.h>
+#include <asm/alternative.h>
+
+/* Our replacement function for xen_hello_world. */
+const char *xen_bye_world(void)
+{
+    return "Bye World!";
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
