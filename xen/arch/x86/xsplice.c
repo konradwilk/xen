15b3819e62142ce2447eeae46d9c39111d1146a5
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-04-11 09:27:22 -0400

    xsplice: Implement payload loading
    Add support for loading xsplice payloads. This is somewhat similar to
    the Linux kernel module loader, implementing the following steps:
    - Verify the elf file.
    - Parse the elf file.
    - Allocate a region of memory mapped within a free area of
      [xen_virt_end, XEN_VIRT_END].
    - Copy allocated sections into the new region. Split them in three
      regions - .text, .data, and .rodata. MUST have at least .text.
    - Resolve section symbols. All other symbols must be absolute addresses.
      (Note that patch titled "xsplice,symbols: Implement symbol name resolution
       on address" implements that)
    - Perform relocations.
    - Secure the the regions (.text,.data,.rodata) with proper permissions.
    
    We capitalize on the vmalloc callback API (see patch titled:
    "rm/x86/vmap: Add vmalloc_xen, vfree_xen and vm_init_type") to allocate
    a region of memory within the [xen_virt_end, XEN_VIRT_END] for the code.
    
    We also use the "x86/mm: Introduce modify_xen_mappings()"
    to change the virtual address page-table permissions.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Acked-by: Julien Grall <julien.grall@arm.com>
    
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Keir Fraser <keir@xen.org>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    
    v2: - Change the 'xsplice_patch_func' structure layout/size.
        - Add more error checking. Fix memory leak.
        - Move elf_resolve and elf_perform relocs in elf file.
        - Print the payload address and pages in keyhandler.
    v3:
        - Make it build under ARM
        - Build it without using the return_ macro.
        - Add fixes from Ross.
        - Add the _return macro back - but only use it during debug builds.
        - Remove the macro, prefix arch_ on arch specific calls.
    v4:
        - Move alloc_payload to arch specific file.
        - Use void* instead of uint8_t, use const
        - Add copyrights
        - Unroll the vmap code to add ASSERT. Change while to not incur
          potential long error loop
       - Use vmalloc/vfree cb APIs
       - Secure .text pages to be RX instead of RWX.
    v5:
      - Fix allocation of virtual addresses only allowing one page to be allocated.
      - Create .text, .data, and .rodata regions with different permissions.
      - Make the find_space_t not a typedef to pointer to a function.
      - Allocate memory in here.
    v6: Drop parentheses on typedefs.
      - s/an xSplice/a xSplice/
      - Rebase on "vmap: Add vmalloc_cb"
      - Rebase on "vmap: Add vmalloc_type and vm_init_type"
      - s/uint8_t/void/ on load_addr
      - Set xsplice_elf on stack without using memset.
    v7:
      - Changed the check on delta = elf->hdr->e_shoff + elf->hdr->e_shnum * elf->hdr->e_shentsize;
        The sections can be right at the back of the file (different linker!), so the failing conditional
        for 'if (delta >= elf->len)' is incorrect and should have been '>'.
      - Changed dprintk(XENLOG_DEBUG to XENLOG_ERR, then back to DEBUG. Converted
        some of the printk to dprintk.
      - Rebase on " arm/x86/vmap: Add vmalloc_xen, vfree_xen and vm_init_type"
      - Changed some of the printk XENLOG_ERR to XENLOG_DEBUG
      - Check the idx in the relocation to make sure it is within bounds and
        implemented.
      - Use "x86/mm: Introduce modify_xen_mappings()"
      - Introduce PRIxElfAddr
      - Check for overflow in R_X86_64_PC32
      - Return -EOPNOTSUPP if we don't support types in ELF64_R_TYPE
    v8:
      - Change dprintk and printk XENLOG_DEBUG to XENLOG_ERR
      - Convert four of the printks in dprintk.
    ---

---
+++ xen/arch/x86/xsplice.c
@@ -0,0 +1,188 @@
+/*
+ * Copyright (C) 2016 Citrix Systems R&D Ltd.
+ */
+
+#include <xen/errno.h>
+#include <xen/lib.h>
+#include <xen/mm.h>
+#include <xen/pfn.h>
+#include <xen/vmap.h>
+#include <xen/xsplice_elf.h>
+#include <xen/xsplice.h>
+
+int arch_xsplice_verify_elf(const struct xsplice_elf *elf)
+{
+
+    const Elf_Ehdr *hdr = elf->hdr;
+
+    if ( hdr->e_machine != EM_X86_64 )
+    {
+        dprintk(XENLOG_ERR, XSPLICE "%s: Unsupported ELF Machine type!\n",
+                elf->name);
+        return -EOPNOTSUPP;
+    }
+
+    return 0;
+}
+
+int arch_xsplice_perform_rel(struct xsplice_elf *elf,
+                             const struct xsplice_elf_sec *base,
+                             const struct xsplice_elf_sec *rela)
+{
+    dprintk(XENLOG_ERR, XSPLICE "%s: SHT_REL relocation unsupported\n",
+            elf->name);
+    return -EOPNOTSUPP;
+}
+
+int arch_xsplice_perform_rela(struct xsplice_elf *elf,
+                              const struct xsplice_elf_sec *base,
+                              const struct xsplice_elf_sec *rela)
+{
+    const Elf_RelA *r;
+    unsigned int symndx, i;
+    uint64_t val;
+    uint8_t *dest;
+
+    if ( !rela->sec->sh_entsize || !rela->sec->sh_size ||
+         rela->sec->sh_entsize != sizeof(Elf_RelA) )
+    {
+        dprintk(XENLOG_ERR, XSPLICE "%s: Section relative header is corrupted!\n",
+                elf->name);
+        return -EINVAL;
+    }
+
+    for ( i = 0; i < (rela->sec->sh_size / rela->sec->sh_entsize); i++ )
+    {
+        r = rela->data + i * rela->sec->sh_entsize;
+        if ( (unsigned long)r > (unsigned long)(elf->hdr + elf->len) )
+        {
+            dprintk(XENLOG_ERR, XSPLICE "%s: Relative entry %u in %s is past end!\n",
+                    elf->name, i, rela->name);
+            return -EINVAL;
+        }
+
+        symndx = ELF64_R_SYM(r->r_info);
+        if ( symndx > elf->nsym )
+        {
+            dprintk(XENLOG_ERR, XSPLICE "%s: Relative symbol wants symbol@%u which is past end!\n",
+                    elf->name, symndx);
+            return -EINVAL;
+        }
+
+        dest = base->load_addr + r->r_offset;
+        val = r->r_addend + elf->sym[symndx].sym->st_value;
+
+        switch ( ELF64_R_TYPE(r->r_info) )
+        {
+        case R_X86_64_NONE:
+            break;
+
+        case R_X86_64_64:
+            *(uint64_t *)dest = val;
+            break;
+
+        case R_X86_64_PLT32:
+            /*
+             * Xen uses -fpic which normally uses PLT relocations
+             * except that it sets visibility to hidden which means
+             * that they are not used.  However, when gcc cannot
+             * inline memcpy it emits memcpy with default visibility
+             * which then creates a PLT relocation.  It can just be
+             * treated the same as R_X86_64_PC32.
+             */
+            /* Fall through */
+        case R_X86_64_PC32:
+            val -= (uint64_t)dest;
+            *(uint32_t *)dest = val;
+            if ( (s64)val != *(s32 *)dest )
+            {
+                dprintk(XENLOG_ERR, XSPLICE "%s: Overflow in relocation %u in %s for %s!\n",
+                        elf->name, i, rela->name, base->name);
+                return -EOVERFLOW;
+            }
+            break;
+
+        default:
+            dprintk(XENLOG_ERR, XSPLICE "%s: Unhandled relocation %lu\n",
+                    elf->name, ELF64_R_TYPE(r->r_info));
+            return -EOPNOTSUPP;
+        }
+    }
+
+    return 0;
+}
+
+/*
+ * The function prepares a xSplice payload by allocating space which
+ * then can be used for loading the allocated sections, resolving symbols,
+ * performing relocations, etc.
+ */
+void *arch_xsplice_alloc_payload(unsigned int pages)
+{
+    unsigned int i;
+    void *p;
+
+    ASSERT(pages);
+
+    p = vmalloc_xen(pages * PAGE_SIZE);
+    WARN_ON(!p);
+    if ( p )
+    {
+        /* By default they are PAGE_HYPERVISOR aka PAGE_HYPERVISOR_RWX.*/
+        for ( i = 0; i < pages; i++ )
+            clear_page(p + (i * PAGE_SIZE) );
+    }
+    return p;
+}
+
+/*
+ * Once the resolving symbols, performing relocations, etc is complete
+ * we secure the memory by putting in the proper page table attributes
+ * for the desired type.
+ */
+int arch_xsplice_secure(void *va, unsigned int pages, enum va_type type)
+{
+    unsigned long start = (unsigned long)va;
+    int flag;
+
+    ASSERT(va);
+    ASSERT(pages);
+
+    if ( type == XSPLICE_VA_RX ) /* PAGE_HYPERVISOR_RX */
+        flag = _PAGE_PRESENT;
+    else if ( type == XSPLICE_VA_RW ) /* PAGE_HYPERVISOR_RW */
+        flag = _PAGE_RW | _PAGE_NX | _PAGE_PRESENT;
+    else /* PAGE_HYPERVISOR_RO */
+        flag = _PAGE_NX | _PAGE_PRESENT;
+
+    /* The ones we are allowed to modify are: _PAGE_NX|_PAGE_RW|_PAGE_PRESENT */
+    modify_xen_mappings(start, start + pages * PAGE_SIZE, flag);
+
+    return 0;
+}
+
+void arch_xsplice_free_payload(void *va)
+{
+    vfree_xen(va);
+}
+
+void arch_xsplice_init(void)
+{
+    void *start, *end;
+
+    start = (void *)xen_virt_end;
+    end = (void *)(XEN_VIRT_END - NR_CPUS * PAGE_SIZE);
+
+    BUG_ON(end <= start);
+
+    vm_init_type(VMAP_XEN, start, end);
+}
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
--- xen/common/xsplice_elf.c
+++ xen/common/xsplice_elf.c
@@ -101,7 +101,7 @@ static int elf_resolve_sections(struct xsplice_elf *elf, const void *data)
             }
 
             elf->symtab = &sec[i];
-
+            elf->symtab_idx = i;
             /*
              * elf->symtab->sec->sh_link would point to the right section
              * but we hadn't finished parsing all the sections.
@@ -249,9 +249,127 @@ static int elf_get_sym(struct xsplice_elf *elf, const void *data)
     return 0;
 }
 
+int xsplice_elf_resolve_symbols(struct xsplice_elf *elf)
+{
+    unsigned int i;
+    int rc = 0;
+
+    /*
+     * The first entry of an ELF symbol table is the "undefined symbol index".
+     * aka reserved so we skip it.
+     */
+    ASSERT(elf->sym);
+
+    for ( i = 1; i < elf->nsym; i++ )
+    {
+        uint16_t idx = elf->sym[i].sym->st_shndx;
+
+        rc = 0;
+        switch ( idx )
+        {
+        case SHN_COMMON:
+            dprintk(XENLOG_ERR, XSPLICE "%s: Unexpected common symbol: %s\n",
+                    elf->name, elf->sym[i].name);
+            rc = -EINVAL;
+            break;
+
+        case SHN_UNDEF:
+            dprintk(XENLOG_ERR, XSPLICE "%s: Unknown symbol: %s\n",
+                    elf->name, elf->sym[i].name);
+            rc = -ENOENT;
+            break;
+
+        case SHN_ABS:
+            dprintk(XENLOG_DEBUG, XSPLICE "%s: Absolute symbol: %s => %#"PRIxElfAddr"\n",
+                    elf->name, elf->sym[i].name, elf->sym[i].sym->st_value);
+            break;
+
+        default:
+            /* SHN_COMMON and SHN_ABS are above. */
+            if ( idx > SHN_LORESERVE )
+                rc = -EOPNOTSUPP;
+            /* SHN_UNDEF (0) above. */
+            else if ( idx > elf->hdr->e_shnum && idx < SHN_LORESERVE )
+                rc = -EINVAL;
+
+            if ( rc )
+            {
+                dprintk(XENLOG_ERR, XSPLICE "%s: Unknown type=%#"PRIx16"\n",
+                        elf->name, idx);
+                break;
+            }
+
+            if ( !(elf->sec[idx].sec->sh_flags & SHF_ALLOC) )
+                break;
+
+            elf->sym[i].sym->st_value += (unsigned long)elf->sec[idx].load_addr;
+            if ( elf->sym[i].name )
+                dprintk(XENLOG_DEBUG, XSPLICE "%s: Symbol resolved: %s => %#"PRIxElfAddr"(%s)\n",
+                       elf->name, elf->sym[i].name,
+                       elf->sym[i].sym->st_value, elf->sec[idx].name);
+        }
+
+        if ( rc )
+            break;
+    }
+
+    return rc;
+}
+
+int xsplice_elf_perform_relocs(struct xsplice_elf *elf)
+{
+    struct xsplice_elf_sec *rela, *base;
+    unsigned int i;
+    int rc = 0;
+
+    /*
+     * The first entry of an ELF symbol table is the "undefined symbol index".
+     * aka reserved so we skip it.
+     */
+    ASSERT(elf->sym);
+
+    for ( i = 1; i < elf->hdr->e_shnum; i++ )
+    {
+        rela = &elf->sec[i];
+
+        if ( (rela->sec->sh_type != SHT_RELA) &&
+             (rela->sec->sh_type != SHT_REL) )
+            continue;
+
+         /* Is it a valid relocation section? */
+         if ( rela->sec->sh_info >= elf->hdr->e_shnum )
+            continue;
+
+         base = &elf->sec[rela->sec->sh_info];
+
+         /* Don't relocate non-allocated sections. */
+         if ( !(base->sec->sh_flags & SHF_ALLOC) )
+            continue;
+
+        if ( rela->sec->sh_link != elf->symtab_idx )
+        {
+            dprintk(XENLOG_ERR, XSPLICE "%s: Relative link of %s is incorrect (%d, expected=%d)\n",
+                    elf->name, rela->name, rela->sec->sh_link, elf->symtab_idx);
+            rc = -EINVAL;
+            break;
+        }
+
+        if ( rela->sec->sh_type == SHT_RELA )
+            rc = arch_xsplice_perform_rela(elf, base, rela);
+        else /* SHT_REL */
+            rc = arch_xsplice_perform_rel(elf, base, rela);
+
+        if ( rc )
+            break;
+    }
+
+    return rc;
+}
+
 static int xsplice_header_check(const struct xsplice_elf *elf)
 {
     const Elf_Ehdr *hdr = elf->hdr;
+    int rc;
 
     if ( sizeof(*elf->hdr) > elf->len )
     {
@@ -275,6 +393,10 @@ static int xsplice_header_check(const struct xsplice_elf *elf)
         dprintk(XENLOG_ERR, XSPLICE "%s: Invalid ELF payload!\n", elf->name);
         return -EOPNOTSUPP;
     }
+
+    rc = arch_xsplice_verify_elf(elf);
+    if ( rc )
+        return rc;
 
     if ( elf->hdr->e_shstrndx == SHN_UNDEF )
     {
