24aa79849b7a5f8b48e2c686a27a90fb53e5e68d
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-04-20 19:40:46 -0400

    xsplice: Add .xsplice.hooks functions and test-case
    Add hook functions which run during patch apply and patch revert.
    Hook functions are used by xsplice payloads to manipulate data structures
    during patching, etc.
    
    One use case is the XSA91. As Martin mentions it:
    "If we have shadow variables, we also need an unload hook to garbage
    collect all the variables introduced by a hotpatch to prevent memory
    leaks.  Potentially, we also want to pre-reserve memory for static or
    existing dynamic objects in the load-hook instead of on the fly.
    
    For testing and debugging, various applications are possible.
    
    In general, the hooks provide flexibility when having to deal with
    unforeseen cases, but their application should be rarely required (<
    10%)."
    
    Furthermore include a test-case for it.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
    ---
    Cc: Keir Fraser <keir@xen.org>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    
    v2: Style guide changes
    v3: Include the test-case - and also re-order this patch
    v6: Make it possible to have multiple XSPLICE_[UN|]LOAD_HOOKS
      - Expand testcase to cover multiple hooks (and imbalanced).
      - Drop paranetheses on typdefs.
      - Move explanation in the design doc a bit.
        Expand a bit
    v7: Check ASSERT after calling hooks.
    ---

--- xen/arch/x86/test/xen_hello_world.c
+++ xen/arch/x86/test/xen_hello_world.c
@@ -4,14 +4,48 @@
  */
 
 #include "config.h"
+#include <xen/lib.h>
 #include <xen/types.h>
 #include <xen/version.h>
 #include <xen/xsplice.h>
+#include <xen/xsplice_patch.h>
 
 #include <public/sysctl.h>
 
 static char hello_world_patch_this_fnc[] = "xen_extra_version";
 extern const char *xen_hello_world(void);
+static unsigned int cnt;
+
+static void apply_hook(void)
+{
+    printk(KERN_DEBUG "Hook executing.\n");
+}
+
+static void revert_hook(void)
+{
+    printk(KERN_DEBUG "Hook unloaded.\n");
+}
+
+static void hi_func(void)
+{
+    printk(KERN_DEBUG "%s: Hi! (called %u times)\n", __func__, ++cnt);
+};
+
+/* If we are sorted we _MUST_ be the last .xsplice.hook section. */
+static void Z_check_fnc(void)
+{
+    BUG_ON(cnt != 2);
+}
+
+XSPLICE_LOAD_HOOK(apply_hook);
+XSPLICE_UNLOAD_HOOK(revert_hook);
+
+/* Imbalance here. Two load and three unload. */
+
+XSPLICE_LOAD_HOOK(hi_func);
+XSPLICE_UNLOAD_HOOK(hi_func);
+
+XSPLICE_UNLOAD_HOOK(Z_check_fnc);
 
 struct xsplice_patch_func __section(".xsplice.funcs") xsplice_xen_hello_world = {
     .version = XSPLICE_PAYLOAD_VERSION,
