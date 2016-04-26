773c0806f476f1e525ffdc8af3545c1e1c82b156
Andrew Cooper <andrew.cooper3@citrix.com>
2016-04-26 13:46:26 +0200

    x86/mm: introduce modify_xen_mappings()
    To simply change the permissions on existing Xen mappings.  The existing
    destroy_xen_mappings() is altered to support changing the PTE permissions.
    
    A new destroy_xen_mappings() is introduced, as the special case of not passing
    _PAGE_PRESENT to modify_xen_mappings().
    
    As cleanup (and an ideal functional test), the boot logic which remaps Xen's
    code and data with reduced permissions is altered to use
    modify_xen_mappings(), rather than map_pages_to_xen() passing the same mfn's
    back in.
    
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Reviewed-by: George Dunlap <george.dunlap@citrix.com>
    Reviewed-by: Jan Beulich <jbeulich@suse.com>
    (cherry picked from commit 36e2f084ecb631838f04d73c6f7dc6bb305d0fe4)
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
     Conflicts:
    	xen/arch/x86/mm.c
    
    [We are not backporting the 2M superpages patches, hence the conflict]
    [Also, there is no _PAGE_NX in 4.4, only _PAGE_NX_BIT]

--- xen/include/xen/mm.h
+++ xen/include/xen/mm.h
@@ -54,6 +54,8 @@ int map_pages_to_xen(
     unsigned long mfn,
     unsigned long nr_mfns,
     unsigned int flags);
+/* Alter the permissions of a range of Xen virtual address space. */
+void modify_xen_mappings(unsigned long s, unsigned long e, unsigned int flags);
 void destroy_xen_mappings(unsigned long v, unsigned long e);
 
 /* Claim handling */
