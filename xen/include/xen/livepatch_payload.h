f30717dbaf15e3ceaad624924e3e830fdbdf1f63
Ross Lagerwall <ross.lagerwall@citrix.com>
2016-06-16 10:48:43 -0400

    livepatch: Add support for shadow variables.
    Shadow variables are a piece of infrastructure to be used by livepatch
    modules. They are used to attach a new piece of data to an existing
    structure in memory.
    
    Martin Pohlack mentions that using small primers in the SHADOW_SLOTS
    has the advantage of having a simple hash function, but cautions
    that using round numbers (such as 256) will give "lot's of hash collisions
    at the price of very fast hash computations as compilers, linkers, and
    memory allocators tend to align starting addresses." (Martin)
    Hence we pick a oddball size.
    
    Signed-off-by: Ross Lagerwall <ross.lagerwall@citrix.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    ---
    v5: http://www.gossamer-threads.com/lists/xen/devel/424811
        XXX
    v12: s/xsplice/livepatch/
    
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: George Dunlap <George.Dunlap@eu.citrix.com>
    Cc: Ian Jackson <ian.jackson@eu.citrix.com>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Tim Deegan <tim@xen.org>
    Cc: Wei Liu <wei.liu2@citrix.com>

--- xen/include/xen/livepatch_payload.h
+++ xen/include/xen/livepatch_payload.h
@@ -36,6 +36,36 @@ typedef void livepatch_unloadcall_t(void);
     livepatch_unloadcall_t *__attribute__((weak)) \
         livepatch_unload_data_##_fn __section(".livepatch.hooks.unload") = _fn;
 
+/*
+ * livepatch shadow variables
+ *
+ * These functions can be used to add new "shadow" fields to existing data
+ * structures.  For example, to allocate a "newpid" variable associated with an
+ * instance of task_struct, and assign it a value of 1000:
+ *
+ * struct task_struct *tsk = current;
+ * int *newpid;
+ * newpid = livepatch_shadow_alloc(tsk, "newpid", sizeof(int));
+ * if (newpid)
+ * 	*newpid = 1000;
+ *
+ * To retrieve a pointer to the variable:
+ *
+ * struct task_struct *tsk = current;
+ * int *newpid;
+ * newpid = livepatch_shadow_get(tsk, "newpid");
+ * if (newpid)
+ * 	printk("task newpid = %d\n", *newpid); // prints "task newpid = 1000"
+ *
+ * To free it:
+ *
+ * livepatch_shadow_free(tsk, "newpid");
+ */
+
+void *livepatch_shadow_alloc(const void *obj, const char *var, size_t size);
+void livepatch_shadow_free(const void *obj, const char *var);
+void *livepatch_shadow_get(const void *obj, const char *var);
+
 #endif /* __XEN_LIVEPATCH_PAYLOAD_H__ */
 
 /*
