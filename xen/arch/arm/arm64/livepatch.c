48604af54b7324dfff0ca3af00322aca131ca78f
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2016-09-09 22:15:55 -0400

    livepatch, arm[32|64]: Share arch_livepatch_revert_jmp
    It is exactly the same in both platforms.
    
    No functional change.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    ---
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    
    v4: New submission.

--- xen/arch/arm/arm64/livepatch.c
+++ xen/arch/arm/arm64/livepatch.c
@@ -53,24 +53,7 @@ void arch_livepatch_apply_jmp(struct livepatch_func *func)
     clean_and_invalidate_dcache_va_range(new_ptr, sizeof(*new_ptr) * len);
 }
 
-void arch_livepatch_revert_jmp(const struct livepatch_func *func)
-{
-    uint32_t *new_ptr;
-    unsigned int i, len;
-
-    new_ptr = func->old_addr - (void *)_start + vmap_of_xen_text;
-    len = arch_livepatch_insn_len(func) / sizeof(uint32_t);
-    for ( i = 0; i < len; i++ )
-    {
-        uint32_t insn;
-
-        memcpy(&insn, func->opaque + (i * sizeof(uint32_t)), PATCH_INSN_SIZE);
-        /* PATCH! */
-        *(new_ptr + i) = insn;
-    }
-
-    clean_and_invalidate_dcache_va_range(func->old_addr, sizeof(*new_ptr) * len);
-}
+/* arch_livepatch_revert_jmp shared with ARM32. */
 
 int arch_livepatch_verify_elf(const struct livepatch_elf *elf)
 {
