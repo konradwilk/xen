3432f0ff91e0fe34392b12d65d836dadf13edae7
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2016-09-10 20:41:24 -0400

    livepatch: arm[32,64],x86: NOP test-case
    The test-case is quite simple - we NOP the 'xen_minor_version'.
    The amount of NOPs depends on the architecture.
    
    On x86 the function is 11 bytes long:
    
       55                      push   %rbp      <- NOP
       48 89 e5                mov    %rsp,%rbp <- NOP
       b8 04 00 00 00          mov    $0x4,%eax <- NOP
       5d                      pop    %rbp      <- NOP
       c3                      retq
    
    We can NOP everything but the last instruction (so 10 bytes).
    
    On ARM64 its 8 bytes:
    
      52800100    mov w0, #0x8         <- NOP
      d65f03c0    ret
    
    We can NOP the first instruction.
    
    While on ARM32 there are 24 bytes:
    
      e52db004        push    {fp}       <- NOP
      e28db000        add     fp, sp, #0 <- NOP
      e3a00008        mov     r0, #8     <- NOP
      e24bd000        sub     sp, fp, #0 <- NOP
      e49db004        pop     {fp}       <- NOP
      e12fff1e        bx      lr
    
    And we can NOP instructions 1 through 5.
    
    Granted this code may be different per compiler!
    
    Hence if anybody does run this test-case - they should
    verify that the assumptions made here are correct.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    ---
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    
    v3: New submission.
    v6: Redo the ARM code to NOP over push/pop as well.

---
+++ xen/test/livepatch/xen_nop.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Oracle and/or its affiliates. All rights reserved.
+ *
+ */
+
+#include "config.h"
+#include <xen/types.h>
+
+#include <public/sysctl.h>
+
+/*
+ * All of the .new_size and .old_addr are based on assumptions that the
+ * code for 'xen_minor_version' is compiled in specific way. Before
+ * running this test-case you MUST verify that the assumptions are
+ * correct (Hint: make debug and look in xen.s).
+ */
+struct livepatch_func __section(".livepatch.funcs") livepatch_nop = {
+    .version = LIVEPATCH_PAYLOAD_VERSION,
+    .old_size = MINOR_VERSION_SZ,
+
+#ifdef CONFIG_X86
+    .old_addr = (void *)MINOR_VERSION_ADDR,
+    /* Everything but the last instruction: "req". */
+    .new_size = MINOR_VERSION_SZ-1,
+#endif
+
+#ifdef CONFIG_ARM_64
+    .old_addr = (void *)MINOR_VERSION_ADDR,
+    /* Replace the first one: "mov w0, #0x8".  */
+    .new_size = 4,
+#endif
+
+#ifdef CONFIG_ARM_32
+    .old_addr = (void *)MINOR_VERSION_ADDR,
+    /* And replace all but the return instruction. */
+    .new_size = MINOR_VERSION_SZ-4,
+#endif
+};
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
