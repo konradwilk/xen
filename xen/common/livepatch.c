4a0fdb59eca47fa5924d4033e47f23e58501ba2a
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2016-09-19 11:12:51 -0400

    arm/x86/common: Add HAS_[ALTERNATIVE|EX_TABLE]
    x86 implements all of them by default - and we just
    add two extra HAS_ variables to be declared in autoconf.h.
    
    ARM 64 only has alternative while ARM 32 has none of them.
    
    And while at it change the livepatch common code that
    would benefit from this.
    
    Acked-by: Jan Beulich <jbeulich@suse.com> [relevant parts]
    Suggested-by: Julien Grall <julien.grall@arm.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
    ---
    Cc: Stefano Stabellini <sstabellini@kernel.org>
    Cc: Julien Grall <julien.grall@arm.com>
    Cc: Jan Beulich <jbeulich@suse.com>
    Cc: Andrew Cooper <andrew.cooper3@citrix.com>
    Cc: Doug Goldstein <cardoe@cardoe.com>
    
    v2: First submission
    v3: Move the config options to common code
        Don't include <xen/config.h> in the file.
        Don't even include <xen/kconfig.h> in the file as xen/Rules.mk automatically
        includes the config.h for every GCC invocation.
    v4: Depend on "arm64: s/ALTERNATIVE/HAS_ALTERNATIVE/"
        Remove the extra newline in arch/x86/Kconfig
    v5: Add Jan's Ack.

--- xen/common/livepatch.c
+++ xen/common/livepatch.c
@@ -643,7 +643,7 @@ static int prepare_payload(struct payload *payload,
                                   sizeof(*region->frame[i].bugs);
     }
 
-#ifndef CONFIG_ARM
+#ifdef CONFIG_HAS_ALTERNATIVE
     sec = livepatch_elf_sec_by_name(elf, ".altinstructions");
     if ( sec )
     {
@@ -674,7 +674,9 @@ static int prepare_payload(struct payload *payload,
         }
         apply_alternatives(start, end);
     }
+#endif
 
+#ifdef CONFIG_HAS_EX_TABLE
     sec = livepatch_elf_sec_by_name(elf, ".ex_table");
     if ( sec )
     {
